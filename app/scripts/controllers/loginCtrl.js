381fcf9db1887adf2308357ae4166db69a55f479
Fernanda Retamal <fpretamal@gmail.com>
2017-06-04 14:27:52 -0400

    login v1

---
+++ app/scripts/controllers/loginCtrl.js
@@ -0,0 +1,21 @@
+angular.module('poliTweets')
+	.controller('loginCtrl', function($location, $scope, loginService){
+
+		$scope.submit = function(){
+
+/*			loginService.getUsuario($scope.user,$scope.pass).then(function(data){
+					$scope.respuesta= data;
+				}, function(error){
+						console.log(error, "noooo error");	
+				});*/
+
+			if($scope.username == "admin" && $scope.password == "admin"){
+				$cookies.put('sesion',$scope.username);				
+				$location.path('/admin');
+				window.location.reload();
+			} else{
+				alert("Usuario o contraseña incorrecto.");
+			}
+		}
+
+	});
