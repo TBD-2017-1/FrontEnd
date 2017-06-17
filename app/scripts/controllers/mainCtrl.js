9563e2a43c5ff453fe7344685bd6bf61c2231da5
kevin-alvarez <kebingo12@hotmail.com>
2017-06-16 21:12:22 -0400

    metodos to<vista>() para acceder a vistas

--- app/scripts/controllers/mainCtrl.js
+++ app/scripts/controllers/mainCtrl.js
@@ -1,3 +1,18 @@
 angular.module('poliTweets')
-		.controller('mainCtrl', function($scope){
+		.controller('mainCtrl', function($scope, $location){
+			$scope.toHome = function(){
+				$location.url('/home');
+			}
+			$scope.toDetailsCongl = function(){
+				$location.url('/aprobacionConglomerados');
+			}
+			$scope.toDetailsPart = function(){
+				$location.url('/aprobacionPartidos');
+			}
+			$scope.toDetailsPolit = function(){
+				$location.url('/aprobacionPoliticos');
+			}
+			$scope.toInfluences = function(){
+				$location.url('/influencias');
+			}
 });
